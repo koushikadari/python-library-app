905d952b15d82d8556e3d3ad5a783b25a4d68eba
koushikadari <103993631+koushikadari@users.noreply.github.com>
2025-12-19 18:13:57 +0400

    Update borrow_service.py

--- borrow/borrow_service.py
+++ borrow/borrow_service.py
@@ -7,7 +7,7 @@ import os
 
 def get_db():
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST", "db"),
+        host=os.getenv("DB_HOST", "database"),
         user=os.getenv("DB_USER", "root"),
         password=os.getenv("DB_PASSWORD", "root"),
         database=os.getenv("DB_NAME", "digital_library")
